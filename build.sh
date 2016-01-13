3a87839a53c0b9f221aa38c7187572c13a3ec1cf
Jim Martens <github@2martens.de>
2016-01-13 18:17:44 +0100

    Ensured that master branch is not built
    Signed-off-by: Jim Martens <github@2martens.de>

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 branch=$(<branchinfo)
 dirname="${branch}bingo.2martens.de"
 
+if [ "$branch" = "master" ]; then
+    exit
+fi
+
 # transport files over to uberspace
 rsync -r --exclude=".git/" --exclude="README" . uberspace:html/$dirname/ 
