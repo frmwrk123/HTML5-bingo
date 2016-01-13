36c0980ec33fdf1eac38e8523a4f050b23f52f4f
Jim Martens <github@2martens.de>
2016-01-13 18:57:57 +0100

    Hopefully fixed rsync
    Signed-off-by: Jim Martens <github@2martens.de>

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ if [ "$branch" = "master" ]; then
 fi
 
 # transport files over to uberspace
-rsync -ar --exclude=".git*" --exclude="README" . uberspace:html/$dirname/ 
+rsync -tvzr --progress --exclude=".git*" --exclude="README" . uberspace:html/$dirname/ 
